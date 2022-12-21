90a42e8ea5e6e778105a48d143bbe2462a53f0d2
Daniel Mateu <danielmateu86@gmail.com>
2022-12-21 16:19:51 +0100

    Introduciendo useCallback + useRef

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,14 +1,51 @@
+import { useCallback, useMemo, useRef } from "react"
+import { MARCAS, PLANES } from "../constants"
 import useCotizador from "../hooks/useCotizador"
-
 
 
 export const Resultado = () => {
 
-    const {resultado} = useCotizador()
+    const { resultado, datos } = useCotizador()
+    const { marca, plan, year } = datos
+    const yearRef = useRef(year)
 
-    if(resultado === 0) return null;
+    const [nombreMarca] = useCallback(
+        MARCAS.filter(m => m.id === Number(marca)),
+        [resultado]
+    );
+
+    const [nombrePlan] = useCallback(
+        PLANES.filter(p => p.id === Number(plan)),
+        [resultado],
+    );
+
+
+
+    if (resultado === 0) return null;
 
     return (
-        <div>{resultado}</div>
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow-lg">
+            <h2 className="text-gray-500 font-black text-3xl">
+                Resumen
+            </h2>
+
+            <p className="my-2">
+                <span className="font-bold">Marca: </span>
+                {nombreMarca.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Año del coche: </span>
+                {yearRef.current}
+            </p>
+            <p className="my-2 text-2xl">
+                <span className="font-bold">Presupuesto total: </span>
+                {resultado}
+            </p>
+        </div>
+
     )
 }
